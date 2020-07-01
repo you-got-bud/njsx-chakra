76a85340e162fd33532baf0e0969fbdfc32a1976
brettm12345 <brettmandler@gmail.com>
2020-07-01 02:12:58 -0400

    Initial commit

--- generate.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var fs_extra_1 = require("fs-extra");
-var path_1 = require("path");
-console.log(fs_extra_1.readdirSync(path_1.join(__dirname, "node_modules", "@chakra-ui", "core", "dist")));
