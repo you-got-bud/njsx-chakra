347380d0fa7b3f2f4c56a7bdaf539edeecae4868
brettm12345 <brettmandler@gmail.com>
2020-07-01 02:15:11 -0400

    Updated .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./njsx-chakra.cjs.production.min.js')
-} else {
-  module.exports = require('./njsx-chakra.cjs.development.js')
-}
