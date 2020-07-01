e05a240144cf2660d0d98452d68108a36736693c
brettm12345 <brettmandler@gmail.com>
2020-07-01 04:39:52 -0400

    Update build setup

---
+++ src/index.d.ts
@@ -0,0 +1,104 @@
+import * as C from '@chakra-ui/core'
+import {Builder} from 'njsx'
+import {PropsOf} from '@chakra-ui/system'
+export declare const Accordion: Builder<PropsOf<typeof C.Accordion>>
+export declare const AccordionItem: Builder<PropsOf<typeof C.AccordionItem>>
+export declare const AccordionHeader: Builder<PropsOf<typeof C.AccordionHeader>>
+export declare const AccordionPanel: Builder<PropsOf<typeof C.AccordionPanel>>
+export declare const AccordionIcon: Builder<PropsOf<typeof C.AccordionIcon>>
+export declare const Alert: Builder<PropsOf<typeof C.Alert>>
+export declare const AlertIcon: Builder<PropsOf<typeof C.AlertIcon>>
+export declare const AlertTitle: Builder<PropsOf<typeof C.AlertTitle>>
+export declare const AlertDescription: Builder<PropsOf<
+  typeof C.AlertDescription
+>>
+export declare const AlertDialog: Builder<PropsOf<typeof C.AlertDialog>>
+export declare const AlertDialogBody: Builder<PropsOf<typeof C.AlertDialogBody>>
+export declare const AlertDialogFooter: Builder<PropsOf<
+  typeof C.AlertDialogFooter
+>>
+export declare const AlertDialogHeader: Builder<PropsOf<
+  typeof C.AlertDialogHeader
+>>
+export declare const AlertDialogContent: Builder<PropsOf<
+  typeof C.AlertDialogContent
+>>
+export declare const AlertDialogOverlay: Builder<PropsOf<
+  typeof C.AlertDialogOverlay
+>>
+export declare const AspectRatioBox: Builder<PropsOf<typeof C.AspectRatioBox>>
+export declare const Avatar: Builder<PropsOf<typeof C.Avatar>>
+export declare const AvatarBadge: Builder<PropsOf<typeof C.AvatarBadge>>
+export declare const AvatarGroup: Builder<PropsOf<typeof C.AvatarGroup>>
+export declare const Badge: Builder<PropsOf<typeof C.Badge>>
+export declare const Box: Builder<PropsOf<typeof C.Box>>
+export declare const Breadcrumb: Builder<PropsOf<typeof C.Breadcrumb>>
+export declare const BreadcrumbItem: Builder<PropsOf<typeof C.BreadcrumbItem>>
+export declare const BreadcrumbLink: Builder<PropsOf<typeof C.BreadcrumbLink>>
+export declare const BreadcrumbSeparator: Builder<PropsOf<
+  typeof C.BreadcrumbSeparator
+>>
+export declare const Button: Builder<PropsOf<typeof C.Button>>
+export declare const ButtonGroup: Builder<PropsOf<typeof C.ButtonGroup>>
+export declare const CSSReset: Builder<PropsOf<typeof C.CSSReset>>
+export declare const Callout: Builder<PropsOf<typeof C.Callout>>
+export declare const Checkbox: Builder<PropsOf<typeof C.Checkbox>>
+export declare const CheckboxGroup: Builder<PropsOf<typeof C.CheckboxGroup>>
+export declare const CircularProgress: Builder<PropsOf<
+  typeof C.CircularProgress
+>>
+export declare const CloseButton: Builder<PropsOf<typeof C.CloseButton>>
+export declare const Code: Builder<PropsOf<typeof C.Code>>
+export declare const Collapse: Builder<PropsOf<typeof C.Collapse>>
+export declare const ColorModeProvider: Builder<PropsOf<
+  typeof C.ColorModeProvider
+>>
+export declare const ControlBox: Builder<PropsOf<typeof C.ControlBox>>
+export declare const Divider: Builder<PropsOf<typeof C.Divider>>
+export declare const Drawer: Builder<PropsOf<typeof C.Drawer>>
+export declare const Editable: Builder<PropsOf<typeof C.Editable>>
+export declare const Flex: Builder<PropsOf<typeof C.Flex>>
+export declare const FormControl: Builder<PropsOf<typeof C.FormControl>>
+export declare const FormErrorMessage: Builder<PropsOf<
+  typeof C.FormErrorMessage
+>>
+export declare const FormHelperText: Builder<PropsOf<typeof C.FormHelperText>>
+export declare const FormLabel: Builder<PropsOf<typeof C.FormLabel>>
+export declare const Grid: Builder<PropsOf<typeof C.Grid>>
+export declare const Heading: Builder<PropsOf<typeof C.Heading>>
+export declare const Icon: Builder<PropsOf<typeof C.Icon>>
+export declare const IconButton: Builder<PropsOf<typeof C.IconButton>>
+export declare const Image: Builder<PropsOf<typeof C.Image>>
+export declare const Input: Builder<PropsOf<typeof C.Input>>
+export declare const InputAddon: Builder<PropsOf<typeof C.InputAddon>>
+export declare const InputGroup: Builder<PropsOf<typeof C.InputGroup>>
+export declare const Kbd: Builder<PropsOf<typeof C.Kbd>>
+export declare const Link: Builder<PropsOf<typeof C.Link>>
+export declare const List: Builder<PropsOf<typeof C.List>>
+export declare const Menu: Builder<PropsOf<typeof C.Menu>>
+export declare const Modal: Builder<PropsOf<typeof C.Modal>>
+export declare const NumberInput: Builder<PropsOf<typeof C.NumberInput>>
+export declare const Popover: Builder<PropsOf<typeof C.Popover>>
+export declare const Portal: Builder<PropsOf<typeof C.Portal>>
+export declare const Progress: Builder<PropsOf<typeof C.Progress>>
+export declare const PseudoBox: Builder<PropsOf<typeof C.PseudoBox>>
+export declare const Radio: Builder<PropsOf<typeof C.Radio>>
+export declare const RadioButtonGroup: Builder<PropsOf<
+  typeof C.RadioButtonGroup
+>>
+export declare const RadioGroup: Builder<PropsOf<typeof C.RadioGroup>>
+export declare const Select: Builder<PropsOf<typeof C.Select>>
+export declare const SimpleGrid: Builder<PropsOf<typeof C.SimpleGrid>>
+export declare const Skeleton: Builder<PropsOf<typeof C.Skeleton>>
+export declare const Slider: Builder<PropsOf<typeof C.Slider>>
+export declare const Spinner: Builder<PropsOf<typeof C.Spinner>>
+export declare const Stack: Builder<PropsOf<typeof C.Stack>>
+export declare const Stat: Builder<PropsOf<typeof C.Stat>>
+export declare const Switch: Builder<PropsOf<typeof C.Switch>>
+export declare const Tabs: Builder<PropsOf<typeof C.Tabs>>
+export declare const Tag: Builder<PropsOf<typeof C.Tag>>
+export declare const Text: Builder<PropsOf<typeof C.Text>>
+export declare const Textarea: Builder<PropsOf<typeof C.Textarea>>
+export declare const ThemeProvider: Builder<PropsOf<typeof C.ThemeProvider>>
+export declare const Tooltip: Builder<PropsOf<typeof C.Tooltip>>
+export declare const VisuallyHidden: Builder<PropsOf<typeof C.VisuallyHidden>>
